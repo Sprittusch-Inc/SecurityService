dc319be50d7e7b8a4e66908b85acc103cda0bc9f
Nikolaj Sievertsen Nøring <94489550+NikoMash@users.noreply.github.com>
2023-05-24 11:49:02 +0200

    Test virker

--- Security.Test/UnitTest1.cs
+++ Security.Test/UnitTest1.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Security.Controllers;
 
 namespace security.test;
@@ -7,43 +10,78 @@ namespace security.test;
 public class Tests
 {
     List<LoginModel> list;
+    private ILogger<AuthenticationController>? logger;
+    private IConfiguration? config;
 
     [SetUp]
     public void Setup()
     {
+        logger = Mock.Of<ILogger<AuthenticationController>>();
+        config = Mock.Of<IConfiguration>();
         list = new List<LoginModel>();
         list.Add(new LoginModel("user@user.com", "password1"));
     }
 
     [Test]
-    public static void invalidLoginEmail()
+    public void invalidLoginEmail()
     {
-        AuthenticationController auth = new AuthenticationController();
-        LoginModel loginmodel = new LoginModel(null, "password1");
-        Assert.IsFalse(auth.Login(loginmodel));
+        try
+        {
+            AuthenticationController auth = new AuthenticationController(logger, config);
+            LoginModel loginmodel = new LoginModel(null, "password1");
+            auth.Login(loginmodel);
+            
+        }
+        catch (Exception ex)
+        {
+            Assert.AreEqual("Value cannot be null. (Parameter 'email')", ex.Message);
+        }
     }
 
     [Test]
-    public void invalidLoginPassword()
+  public void invalidLoginPassword()
     {
-        AuthenticationController auth = new AuthenticationController();
-        LoginModel loginmodel = new LoginModel("user@user.com", null);
-        Assert.IsFalse(auth.Login(loginmodel));
+        try
+        {
+            AuthenticationController auth = new AuthenticationController(logger, config);
+            LoginModel loginmodel = new LoginModel("user1", null);
+            auth.Login(loginmodel);
+            
+        }
+        catch (Exception ex)
+        {
+            Assert.AreEqual("Value cannot be null. (Parameter 'password')", ex.Message);
+        }
     }
 
     [Test]
     public void invalidLogin()
     {
-        AuthenticationController auth = new AuthenticationController();
-        LoginModel loginmodel = new LoginModel(null, null);
-        Assert.IsFalse(auth.Login(loginmodel));
+        try
+        {
+            AuthenticationController auth = new AuthenticationController(logger, config);
+            LoginModel loginmodel = new LoginModel(null, null);
+            auth.Login(loginmodel);
+            
+        }
+        catch (Exception ex)
+        {
+            Assert.AreEqual("Value cannot be null. (Parameter 'email', Parameter 'password')", ex.Message);
+        }
     }
 
     [Test]
     public void validLogin()
     {
-        AuthenticationController auth = new AuthenticationController();
-        LoginModel loginmodel = new LoginModel("user@user.com", "password1");
-        Assert.IsTrue(auth.Login(loginmodel));
+        AuthenticationController auth = new AuthenticationController(logger, config);
+        LoginModel loginmodel = new LoginModel("newuser@user.com", "password2");
+        list.Add(loginmodel);
+        auth.Login(loginmodel);
+
+        Assert.IsTrue(list.Count == 2);
+        Assert.AreEqual(list[1], loginmodel);
+        Assert.IsTrue(list[1].Email == loginmodel.Email); 
+        Assert.IsTrue(list[1].Password == loginmodel.Password);
     }
+    
 }
